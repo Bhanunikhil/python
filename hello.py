6389cd33049b5406caf92ebee4d0cd81591499ba
Bhanunikhil <guntumadugunikhil@gmail.com>
2024-06-19 11:47:42 -0400

    pushing existing files

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("this is my first python program")
+
+age= 21
+print(age)
+print(type(age))
+
+print("my age is :" + str(age))
+
+txt = "nikhil"
+
+print(type(txt))
+print(txt.lower())
+print(txt.upper())
+print(txt.isalpha())
+print(txt.isdigit())
+print(txt.replace("i", "A"))
---
+++ slicing.py
@@ -0,0 +1,10 @@
+txt= "My name is Nikhil"
+
+slicing = txt[0]
+print(slicing)
+
+slicing = txt[0:2]
+print(slicing)
+
+slicing = txt[0:3]
+print(slicing)

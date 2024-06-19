5bdeed8ebb95fe2c6f2d0f94ee9b5354b29bb410
Bhanunikhil <guntumadugunikhil@gmail.com>
2024-06-19 12:41:22 -0400

    commiting after modifing slicing.py completely

--- slicing.py
+++ slicing.py
@@ -7,14 +7,22 @@ txt= "My name is Nikhil"
 slicing = txt[0]
 print(slicing)
 
+#substring from starting index to a particular index
 slicing = txt[0:2]
 print(slicing)
 
+#substring from starting index 0 to a particular index and I am checking here whether it consider a space as an index or not.
 slicing = txt[0:3]
 print(slicing)
 
 slicing = txt[2:7]
 print(slicing)
 
+#substring from start to end index and the last part .step. is like which index should it consider in every iteration 
 slicing =txt[0:17:2]
 print(slicing)
+
+#reversing a string using slicing
+
+reverse_txt = txt[::-1]
+print(reverse_txt)
